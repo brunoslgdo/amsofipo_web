061972eadf602e3ef0f5b1aa36c547bd1464ed12
brunoslgdo <brunoslgdo@gmail.com>
2019-12-26 11:19:30 -0600

    Se crean las carpetas y archivos para el 'flexslider'.

---
+++ js/slider.js
@@ -0,0 +1,9 @@
+$(window).load(function() {
+    $('.flexslider').flexslider({
+      animation: "slide",
+      controlsContainer: $(".custom-controls-container"),
+      customDirectionNav: $(".custom-navigation a"),
+      slideshowSpeed: 5000,
+      animationSpeed: 600
+    });
+  });
