6ad32c001d9c3253bbc743b0d001828bc78b7e19
brunoslgdo <brunoslgdo@gmail.com>
2020-04-18 19:48:52 -0500

    se elmiman todos los archivos del proyecto ya que se integrara la rama EXPERIMENTAL a la rama master

--- js/slider.js
+++
@@ -1,9 +0,0 @@
-$(window).load(function() {
-    $('.flexslider').flexslider({
-      animation: "slide",
-      controlsContainer: $(".custom-controls-container"),
-      customDirectionNav: $(".custom-navigation a"),
-      slideshowSpeed: 5000,
-      animationSpeed: 600
-    });
-  });
