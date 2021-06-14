4f2ee5f372a20280847de367e6e09e11a9a8454b
bruno salgado <brunoslgdo@gmail.com>
2021-06-14 11:27:01 -0500

    limpieza de archivos para la nueva version

--- dist/js/menu.js
+++
@@ -1,8 +0,0 @@
-function menu() {
-    var x = document.getElementById("menu");
-    if (x.style.display === "block") {
-      x.style.display = "none";
-    } else {
-      x.style.display = "block";
-    }
-  }
